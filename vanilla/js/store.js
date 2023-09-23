2a66c5179200527c1410c6ffda97fceef47dcfef
Jack <jackpender9@gmail.com>
2023-09-24 00:04:20 +0100

    Initial project. Only using HTML, CSS and JavaScript

---
+++ vanilla/js/store.js
@@ -0,0 +1,151 @@
+// REPRESENTS THE MODEL
+
+const initialValue = {
+  currentGameMoves: [],
+  history: {
+    currentRoundGames: [],
+    allGames: [],
+  },
+};
+
+export default class Store extends EventTarget {
+  #state = initialValue;
+
+  constructor(key, players) {
+    super(); //allows you to inherit from EventTarget
+    this.storageKey = key;
+    this.players = players;
+  }
+
+  get stats() {
+    // console.log(this.#getState());
+
+    const state = this.#getState();
+
+    return {
+      playerWithStats: this.players.map((player) => {
+        const wins = state.history.currentRoundGames.filter(
+          (game) => game.status.winner?.id === player.id
+        ).length;
+
+        return {
+          ...player,
+          wins,
+        };
+      }),
+
+      ties: state.history.currentRoundGames.filter(
+        (game) => game.status.winner === null
+      ).length,
+    };
+  }
+
+  //   Getter means you don't have to call this as a function in app.js
+  get game() {
+    const state = this.#getState();
+
+    const currentPlayer = this.players[state.currentGameMoves.length % 2];
+
+    const winningPatterns = [
+      [1, 2, 3],
+      [1, 5, 9],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 5, 7],
+      [3, 6, 9],
+      [4, 5, 6],
+      [7, 8, 9],
+    ];
+
+    let winner = null;
+
+    for (const player of this.players) {
+      const selectedSquareIds = state.currentGameMoves
+        .filter((move) => move.player.id === player.id)
+        .map((move) => move.squareId);
+
+      for (const pattern of winningPatterns) {
+        if (pattern.every((v) => selectedSquareIds.includes(v))) {
+          winner = player;
+        }
+      }
+    }
+
+    return {
+      moves: state.currentGameMoves,
+      currentPlayer,
+      status: {
+        isComplete: winner != null || state.currentGameMoves.length === 9,
+        winner,
+      },
+    };
+  }
+
+  playerMove(squareId) {
+    const stateClone = structuredClone(this.#getState());
+
+    stateClone.currentGameMoves.push({
+      squareId,
+      player: this.game.currentPlayer,
+    });
+
+    this.#saveState(stateClone);
+  }
+
+  reset() {
+    const stateClone = structuredClone(this.#getState());
+
+    const { status, moves } = this.game;
+
+    if (status.isComplete) {
+      stateClone.history.currentRoundGames.push({
+        moves,
+        status,
+      });
+    }
+
+    stateClone.currentGameMoves = [];
+
+    this.#saveState(stateClone);
+  }
+
+  newRound() {
+    this.reset();
+
+    const stateClone = structuredClone(this.#getState());
+
+    stateClone.history.allGames.push(...stateClone.history.currentRoundGames);
+    stateClone.history.currentRoundGames = [];
+
+    this.#saveState(stateClone);
+  }
+
+  #getState() {
+    // return this.#state;
+    const item = window.localStorage.getItem(this.storageKey);
+    return item ? JSON.parse(item) : initialValue;
+  }
+
+  #saveState(stateOrFunction) {
+    const prevState = this.#getState();
+
+    let newState;
+
+    switch (typeof stateOrFunction) {
+      case "function":
+        newState = stateOrFunction(prevState);
+        break;
+      case "object":
+        newState = stateOrFunction;
+        break;
+      default:
+        throw new Error("Invalid argument passed to saveState");
+    }
+
+    // this.#state = newState;
+
+    window.localStorage.setItem(this.storageKey, JSON.stringify(newState));
+
+    this.dispatchEvent(new Event("statechange"));
+  }
+}
